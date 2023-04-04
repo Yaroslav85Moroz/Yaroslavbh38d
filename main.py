75eb871620f2705f16d5a972dc4549f6c1c0cae7
CharnyshSv <sv.charnysh@gmail.com>
2023-04-04 12:33:49 +0300

    Revert "add print"
    This reverts commit ee817966

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 text = input()
 first_space = text.find(' ')
 last_space = text.rfind(' ')
+
+first_word = text[:first_space]
+last_word = text[last_space+1]
+center = text[first_space:last_space+1]
+
+result = last_word + center + first_word
+print(result)
